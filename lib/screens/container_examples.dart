9406ab0b082d19331f86fead9f248fccc39517e3
Uttam Bhagaur <uttambhagaur@gmail.com>
2022-10-12 22:19:58 +0530

    container examples complete

--- lib/screens/container_examples.dart
+++ lib/screens/container_examples.dart
@@ -67,6 +67,60 @@ class ContainerWidgets extends StatelessWidget {
     child: Image.network("https://picsum.photos/500/400"),
   );
 
+  Widget container6 = Container(
+    height: 100.0,
+    width: 200.0,
+    decoration: const BoxDecoration(
+      color: Colors.grey,
+      boxShadow: [
+        BoxShadow(color: Colors.red, blurRadius: 12.0),
+        BoxShadow(color: Colors.yellow, blurRadius: 40.0),
+      ],
+    ),
+  );
+
+  Widget container7 = Container(
+    height: 200.0,
+    width: 200.0,
+    margin: const EdgeInsets.all(10.0),
+    decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(50.0),
+        image: const DecorationImage(
+            image: NetworkImage("https://picsum.photos/200/300"),
+            fit: BoxFit.fill)),
+  );
+
+  Widget container8 = Container(
+    height: 200.0,
+    width: 200.0,
+    alignment: Alignment.center,
+    margin: const EdgeInsets.all(10.0),
+    decoration: BoxDecoration(
+      color: Colors.green,
+      borderRadius: BorderRadius.circular(100.0),
+    ),
+    child: const Text('Hello Flutter'),
+  );
+
+  Widget container9 = Container(
+    height: 200.0,
+    width: 200.0,
+    margin: const EdgeInsets.all(20.0),
+    decoration: const BoxDecoration(
+        color: Colors.cyanAccent,
+        borderRadius: BorderRadius.horizontal(
+            left: Radius.circular(10.0), right: Radius.circular(100.0))),
+  );
+
+  Widget container10 = Container(
+    height: 200.0,
+    width: 200.0,
+    decoration: const BoxDecoration(
+      color: Colors.purple,
+        borderRadius: BorderRadius.vertical(
+            bottom: Radius.elliptical(100, 150), top: Radius.circular(20.0))),
+  );
+
   @override
   Widget build(BuildContext context) {
     return ListView(
@@ -76,6 +130,11 @@ class ContainerWidgets extends StatelessWidget {
         container3,
         container4,
         container5,
+        container6,
+        container7,
+        container8,
+        container9,
+        container10,
       ],
     );
   }
